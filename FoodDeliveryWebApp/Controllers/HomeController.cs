29337fac7b97e4a2301c24931aa0a8416100c4de
Albert4940 <dorcealbertmary@gmail.com>
2024-01-09 10:02:03 -0800

    feat:FDW-49456049-Add login form

---
+++ FoodDeliveryWebApp/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+using FoodDeliveryWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace FoodDeliveryWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        Uri baseAdd = new Uri("https://localhost:44339/api");
+        private readonly HttpClient _client;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _client = new HttpClient();
+            _client.BaseAddress = baseAdd;
+        }
+
+        public IActionResult Index()
+        {
+            User user = GetCurrentUser();
+            if(user != null)
+                return View(user);
+            return View();
+        }
+
+        public User GetCurrentUser()
+        {
+            var accessToken = HttpContext.Session.GetString("JWToken");
+
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            HttpResponseMessage response = client.GetAsync(_client.BaseAddress + "/user/admin").Result;
+            User user = null;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                user = JsonConvert.DeserializeObject<User>(data);
+            }
+            return user;
+            /*string jsonStr = await client.GetStringAsync(url);
+            var res = JsonConvert.DeserializeObject<string>(jsonStr);
+            ViewData["res"] = accessToken;
+            return Redirect("~/Dashboard/Index");*/
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FoodDeliveryWebApp/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using FoodDeliveryWebApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace FoodDeliveryWebApp.Controllers
+{
+
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> LoginUser(User user)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                StringContent stringContent = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+                using (var response = await httpClient.PostAsync("https://localhost:44339/api/Auth", stringContent))
+                {
+                    string token = await response.Content.ReadAsStringAsync();
+                    if (token == "Invalid credentials")
+                    {
+                        ViewBag.Message = "Incorrect UserId or Password!";
+                        return Redirect("~/Home/Index");
+                    }
+
+                    HttpContext.Session.SetString("JWToken", token);
+                    ViewData["res"] = token;
+                }
+                return Redirect("~/Home/Index");
+            }
+        }
+    }
+}
