50c116841445c81cfe444485fcdd61450650bf96
Albert4940 <dorcealbertmary@gmail.com>
2024-01-12 23:14:07 -0800

    feat:FDA-49853761-Add  category Service and Routes for Create and Update Operations

---
+++ FoodDeliveryAPI/Controllers/CategoryController.cs
@@ -0,0 +1,96 @@
+﻿using FoodDeliveryAPI.Data;
+using FoodDeliveryAPI.Models;
+using FoodDeliveryAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FoodDeliveryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public CategoryController(FoodDeliveryContext context)
+        {            
+            CategoryService.InitializeContext(context);
+        }
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Category cat)
+        {
+            var category = cat;
+            if(cat.Title != null)
+            {
+                bool categoryExists = await CategoryService.CheckIfCategoryExists(category);
+
+                 if (categoryExists)
+                     return Conflict();
+
+                 await CategoryService.Add(cat);
+                return Ok("Category Created");
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Category cat)
+        {
+            if (id != cat.Id)
+                return BadRequest();
+
+            //To avoid to put  the name that already exists 
+            //Put the title unique in database
+           /* bool categoryExists = await CategoryService.CheckIfCategoryExistsForUpdate(cat);
+
+            if (categoryExists)
+                return Conflict();*/
+            //_context.Entry(cat).State = EntityState.Modified;
+
+            try
+            {
+                await CategoryService.Update(cat);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //Check If Category is in DB
+                var existingCatByID = await CategoryService.GetByID(cat.Id);
+                if (existingCatByID is null)
+                    return NotFound();
+                else
+                {
+                    throw;
+                }
+            }
+            
+
+            return NoContent();
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FoodDeliveryAPI/Services/CategoryService.cs
@@ -0,0 +1,48 @@
+﻿using FoodDeliveryAPI.Data;
+using FoodDeliveryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDeliveryAPI.Services
+{
+    public class CategoryService
+    {
+        //Generics class Services for entity
+        private static FoodDeliveryContext _context;
+        // Method to initialize the context
+        public static void InitializeContext(FoodDeliveryContext context)
+        {
+            _context = context;
+        }
+
+
+        /*public static User? Get(User user) => Users.FirstOrDefault(x => x.UserName.Equals(user.UserName, StringComparison.CurrentCultureIgnoreCase)
+            && x.Password == user.Password);*/
+        public static async Task<Category> Get(Category category) => await _context.categories.FirstOrDefaultAsync(c => c.Title.Equals(category.Title));
+        public static async Task<Category>? GetByID(int? Id) => await _context.categories.FirstOrDefaultAsync(c => c.Id == Id);
+
+        public static async Task Add(Category category)
+        {
+            //That generate by database    
+            category.Id = 0;
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public static async Task Update(Category cat)
+        {
+            //That generate by database 
+            _context.Entry(cat).State = EntityState.Modified;
+            _context.Update(cat);
+            await _context.SaveChangesAsync();
+        }
+
+        //Factorize it inside the intreface
+        public static async Task<bool> CheckIfCategoryExists(Category category) => await CategoryService.Get(category) != null;
+        public static async Task<bool> CheckIfCategoryExistsForUpdate(Category category) {
+            var catResult = await CategoryService.Get(category);
+            if (catResult != null && category.Id != catResult.Id)
+                return true;
+            return false;
+            }
+    }
+}
