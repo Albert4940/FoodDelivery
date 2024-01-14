ba34c414f42acb54f8ae4bf2bfc463097937e8f2
Albert4940 <dorcealbertmary@gmail.com>
2024-01-14 09:34:32 -0800

    feat:FDA-49853761-Add CRUD operations for food

--- FoodDeliveryAPI/Controllers/FoodController.cs
+++ FoodDeliveryAPI/Controllers/FoodController.cs
@@ -1,7 +1,9 @@
 ﻿using FoodDeliveryAPI.Data;
 using FoodDeliveryAPI.Models;
 using FoodDeliveryAPI.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,6 +17,7 @@ namespace FoodDeliveryAPI.Controllers
         public FoodController(FoodDeliveryContext context)
         {
             FoodService.InitializeContext(context);
+            CategoryService.InitializeContext(context);
         }
         // GET: api/<FoodController>
         [HttpGet]
@@ -25,42 +28,82 @@ namespace FoodDeliveryAPI.Controllers
 
         // GET api/<FoodController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Food>> Get(int id)
         {
-            return "value";
+            var food = await FoodService.GetByID(id);
+
+            if (food is null)
+                return NotFound();
+
+            return Ok(food);
         }
 
         // POST api/<FoodController>
         [HttpPost]
+        
         public async Task<IActionResult> Post([FromBody] Food food)
         {
-           /* var category = cat;
-            if (cat.Title != null)
+           // var category = cat;
+            if (food.Title != null && food.CategoryId != 0 && await CategoryService.GetByID(food.CategoryId) != null)
             {
-                bool categoryExists = await CategoryService.CheckIfCategoryExists(category);
+                bool foodExists = await FoodService.CheckIfFoodExists(food);
 
-                if (categoryExists)
-                    return Conflict();*/
+                if (foodExists)
+                    return Conflict();
 
                 await FoodService.Add(food);
                 return Ok("Food Created");
-           /* }
+            }
             else
             {
                 return BadRequest();
-            }*/
+            }
         }
 
         // PUT api/<FoodController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] Food food)
         {
+            if (id != food.Id)
+                return BadRequest();
+
+            if (await CategoryService.GetByID(food.CategoryId) is null)
+                return BadRequest();
+;
+            bool foodExists = await FoodService.CheckIfFoodExistsForUpdate(food);
+
+            if (foodExists)
+                return Conflict();
+
+            try
+            {
+                await FoodService.Update(food);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //Check If Category is in DB
+                var existingFoodByID = await FoodService.GetByID(food.Id);
+                if (existingFoodByID is null)
+                    return NotFound();
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
         }
 
         // DELETE api/<FoodController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            var food = await FoodService.GetByID(id);
+            if (food is null)
+                return NotFound();
+
+            await FoodService.Delete(food);
+
+            return NoContent();
         }
     }
 }
