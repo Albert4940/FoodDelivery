11d0f6eca290195e767f9c04a8b7fdc5c20d5d47
Albert4940 <dorcealbertmary@gmail.com>
2024-01-18 12:40:12 -0800

    feat:FDA-49945604- Add Cart controller

--- FoodDeliveryWebApp/wwwroot/js/site.js
+++ FoodDeliveryWebApp/wwwroot/js/site.js
@@ -8,8 +8,20 @@ jQuery(document).ready(function () {
         ? JSON.parse(localStorage.getItem('cart'))
         : { cartItems: [], shippingAddress: "", paymentMethod: "PayPal" };
 
+
     updateBadge(cart)
 
+    if (cart.cartItems.length > 0) {
+        $.post("/Cart/Index", cart).done(function (data) {
+            console.log("Successfully : ",data)
+        }).fail(function (error) {
+            console.error("Failed  : ",error)
+        })
+    }
+
+    listCart(cart);
+
+    
     $('.btn-add').click(function (e) {
         var btn = $(this);
         
@@ -37,6 +49,29 @@ jQuery(document).ready(function () {
 
     })
 
+    function liCreator(data) {
+        let liElt = "<li class='list-group-item'>"
+            liElt += "<div class='row'>"
+                liElt += "<div class='col-md-2'>"
+                     liElt += "<img src='~/img/c1.png'/>" 
+                liElt += "</div>"
+            liElt += "</div>"
+        liElt += "</li>"
+        return liElt;
+    }
+    function listCart(cart) {
+        //cart.cartItems.length !== 0 ? $('.alert').toggle('block') : $('.list-group').toggle('display','block'); 
+        if (cart.cartItems.length === 0)
+            $('#cart-alert').css('display', 'block')
+        else {
+            $('#cart-list').css('display', 'block')
+            $('#cart-list').html(liCreator())
+        }
+
+
+        /*$('#cart-alert').css('display', 'block')
+        $('.list-group').css('display', 'block')*/
+    }
     function updateCart(cart) {
         localStorage.setItem('cart', JSON.stringify(cart));
     }
