c98b198db664f23f3f173d14b6a509a9824e1f96
Albert4940 <dorcealbertmary@gmail.com>
2024-03-12 14:01:22 -0700

    refactor:FDA-56089431-Refactor Order

---
+++ FoodDeliveryWebApp/Migrations/20240312205642_AddDates.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodDeliveryWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "ShippingAddresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "ShippingAddresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "OrderItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "OrderItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "ShippingAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "ShippingAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "OrderItems");
+        }
+    }
+}
