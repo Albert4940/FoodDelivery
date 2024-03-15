67e6653d9923c38c9882beb57e0dd88fd8966c69
Albert4940 <dorcealbertmary@gmail.com>
2024-03-15 14:13:55 -0700

    Refactor:FDA-56480065:Add Tax and deliveryfee to order

--- FoodDeliveryAPI/Models/Configuration.cs
+++ FoodDeliveryAPI/Models/Configuration.cs
@@ -5,5 +5,7 @@
         public string PayPalCientId { get; set; } = "";
         public string PayPalSecret { get; set; } = "";
         public string PayPalUrl { get; set; } = "";
+        public decimal DeliveryFee { get;  set; }
+        public int TaxPercent { get; set; }
     }
 }
--- FoodDeliveryAPI/Models/Order.cs
+++ FoodDeliveryAPI/Models/Order.cs
@@ -7,8 +7,9 @@ namespace FoodDeliveryWebApp.Models
         public long Id { get; set; }
         public string UserId { get; set; } = "string";
 
-        public double ItemsPrice { get; set; }
-        public double TaxPrice { get; set; }        
+        public decimal ItemsPrice { get; set; }
+        public decimal TaxPrice { get; set; } 
+        public decimal DeliveryFee { get; set; }
         public decimal TotalPrice { get; set; }
 
         public bool IsPaid { get; set; } = false;
